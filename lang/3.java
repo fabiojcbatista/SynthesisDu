97b9f21c877f82cd6f420e4b10cb4ce5e2d1c860
ddzbxh <duanshuxunhuan@outlook.com>
2021-12-08 12:41:18 +0800

    .

---
+++ lang/3.java
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<!-- saved from url=(0042)http://munity.cn/index.aspx?id=UDCZGYcOKkC -->
+<html xmlns="http://www.w3.org/1999/xhtml" style="" class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"><title>
+	自助下载系统
+</title><link rel="stylesheet" media="screen" href="./自助下载系统_files/pa_login.css"><link href="./自助下载系统_files/font-awesome.min.css" rel="stylesheet" type="text/css"><link href="./自助下载系统_files/sweetalert2.css" rel="stylesheet" type="text/css"><link href="./自助下载系统_files/index1.css" rel="stylesheet" type="text/css"><link href="./自助下载系统_files/style1.css" rel="stylesheet" type="text/css">
+    <script src="./自助下载系统_files/jquery.js.下载"></script>
+    <script src="./自助下载系统_files/sweetalert2.min.js.下载"></script>
+    <script src="./自助下载系统_files/clipboard.min.js.下载"></script>
+    <script src="./自助下载系统_files/index1.js.下载"></script>
+    <script src="./自助下载系统_files/isie.js.下载"></script>
+
+
+    <script src="./自助下载系统_files/jquery-1.9.1.js.下载"></script>
+    <script src="./自助下载系统_files/modernizr.js.下载"></script>
+    <script src="./自助下载系统_files/global.js.下载"></script>
+    <script src="./自助下载系统_files/jquery.form.min.js.下载"></script>
+    <script src="./自助下载系统_files/layui.js.下载"></script>
+    <script src="./自助下载系统_files/layui-mz-min.js.下载"></script>
+    <script src="./自助下载系统_files/fuzhu.js.下载"></script>
+    <script src="./自助下载系统_files/clipboard.min.js(1).下载"></script>
+    <style>
+        .table_box {
+            background:#000;
+        }
+    </style>
+</head>
+<body>
+    <form method="post" action="http://munity.cn/index.aspx?id=UDCZGYcOKkC" id="form1">
+<div class="aspNetHidden">
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwULLTIwNTMzODQyNDAPZBYCAgMPZBYCZg8WAh4LXyFJdGVtQ291bnRmZGStdibVgtiNuvg+VIqNtX1FD6za1Wq+2rtVOulZiW7GNw==">
+</div>
+
+<div class="aspNetHidden">
+
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="90059987">
+</div>
+
+        
+        <div id="particles-js" style="display:flex; align-items: center; justify-content: center">
+        <canvas class="particles-js-canvas-el" width="1858" height="977" style="width: 100%; height: 100%;"></canvas></div>
+        
+        <!-- scripts -->
+        <script src="./自助下载系统_files/particles.js.下载"></script>
+        <script src="./自助下载系统_files/app.js.下载"></script>
+        
+        <div class="header">
+
+            <div class="logo_box">
+
+                <a class="logo">自助下载系统</a>
+            </div>
+            <div class="user_box">
+                <div class="img"><i class="fa fa-user-secret fa-3x" aria-hidden="true"></i></div>
+                <div class="name"></div>
+
+            </div>
+        </div>
+
+        <div class="download_info">
+            <div class="inner">
+                <div class="list_box">
+                    <div class="item" style="background: rgb(97, 100, 193,0.7);">
+                        <div class="title">
+如有侵犯到您的权益，请联系管理员下架，邮箱 wfscbw@163.com
+</div>
+                        <div class="num">联系我们</div>
+                        <div class="icon"><i class="fa fa-database fa-5x" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="item" style="background: rgb(88, 88, 88,0.7);">
+                        <div class="title">1.适用于常规文件自助下载<br>
+                            2.大文件下载请耐心等候<br>
+                            3.其他问题请联系商家处理</div>
+                        <div class="num">客服微信：honglin015</div>
+                        <div class="icon"><i class="fa fa-arrow-circle-down fa-5x" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="item" style="background: rgb(234, 78, 3,0.7);">
+                        <div class="title">本站仅提供临时代下载服务。同时也推荐购买官网会员，学习更多的知识，谢谢理解与配合！</div>
+                        <div class="num">声明</div>
+                        <div class="icon"><i class="fa fa-clock-o fa-5x" aria-hidden="true"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="search_box">
+            <div class="inner">
+                <div class="logo"><span>文件下载</span></div>
+                <div class="title">请输入文档页面地址链接地址即可快速下载</div>
+                <div class="form_box">
+                        <div class="input_box">
+                            <div class="label">下载秘钥</div>
+                            <input id="cdk" class="input" type="text" name="url" placeholder="请输入下载秘钥CDKEY">
+                        </div>
+                        <div class="input_box">
+                            <div class="label">网页地址</div>
+                            <input id="data_url" class="input" type="text" name="email" placeholder="请输入下载网页地址" value="">
+                        </div>
+                        <button class="btn" type="button" onclick="download()">下载文件</button>
+                        <button class="btn_history" type="button" onclick="javascript:location.href=&#39;index.aspx?id=&#39;+$(&#39;#cdk&#39;).val();">历史记录</button>
+                </div>
+            </div>
+        </div>
+        <div class="table_box">
+            <div class="inner">
+                <div class="list_box">
+                    <div class="thead">
+                        <div class="tr">
+                            <div class="item">订单编号</div>
+                            <div class="item">下载地址</div>
+                            <div class="item">下载时间</div>
+                            <div class="item">当前状态</div>
+                            <div class="item">反馈结果</div>
+                            <div class="item">保存至本地</div>
+                        </div>
+                    </div>
+                    <div class="tbody">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        
+	<div style="text-align:center;font-size: 12px;color: #eee;z-index:999999;">
+		<a href="http://beian.miit.gov.cn/" target="_blank">渝ICP备16000347号-1</a>
+	</div>
+        
+        <script>
+            var cdkid = req("id");
+            if (cdkid != "") {
+                $("#cdk").val(cdkid);
+            }
+
+            function download() {
+                if ($("#tqButton").hasClass("is-disabled")) {
+                    return;
+                }
+                $("#tqButton").addClass("is-disabled");
+                $("#loading").show();
+
+                var psotdata = {};
+                psotdata["cdk"] = $("#cdk").val();
+                psotdata["source_url"] = $("#data_url").val();
+                $.ajax({
+                    url: '../kk/info.aspx?do=kkCreate',
+                    type: 'POST',
+                    dataType: "json",
+                    data: psotdata,
+                    success: function (json) {
+                        if (json.code == 1) {
+                            alert(json.msg);
+                            location.href = "index.aspx?id=" + json.cdk + "&source_id=" + json.id;
+                        } else {
+                            alert(json.msg);
+                        }
+                        //$("#tqButton").removeClass("is-disabled");
+                        //$("#loading").hide();
+                    },
+                    error: function () {
+                        //$("#tqButton").removeClass("is-disabled");
+                        //$("#loading").hide();
+                    }
+                })
+            }
+
+            function feedback(id) {
+                if (confirm("是否进行文件报错？")) {
+                    var psotdata = {};
+                    psotdata["cdk"] = req("id");
+                    psotdata["id"] = id;
+                    $.ajax({
+                        url: '../kk/info.aspx?do=kkUploadCheck',
+                        type: 'POST',
+                        dataType: "json",
+                        data: psotdata,
+                        success: function (json) {
+                            if (json.code == 1) {
+                                alert(json.msg);
+                            } else {
+                                alert(json.msg);
+                            };
+                        },
+                        error: function () {
+                        }
+                    })
+                }
+            }
+        </script>
+
+    </form>
+
+
+</body></html>
